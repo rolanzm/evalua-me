09f8a07129a6ee619813c2d4ecfc52aac05705bc
rtejo_urp <ricardotejo-urp@outlook.com>
2015-10-15 04:12:30 +0000

    adding course, topic and routes for the REST PoC in #1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( myscripts.js )
+#Rails.application.config.assets.paths << Rails.root.join("lib", "javascripts")
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
